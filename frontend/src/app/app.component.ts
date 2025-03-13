52b3a324f240a4c0ce360da5d28ba091e7929581
turki-aloufi <turkialoufi44@gmail.com>
2025-03-13 14:51:20 +0300

    create angular project structure

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './product/product.component'
+import { OrderComponent } from './order/order.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductComponent, OrderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
